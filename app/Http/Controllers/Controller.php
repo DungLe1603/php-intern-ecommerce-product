dbde40fc53ba38960a4bc0ce75df258bd3f07048
luongthean <luongthean123@gmail.com>
2020-04-21 11:21:11 +0700

    test

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,6 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public static $TEST = "this is test";
 }
