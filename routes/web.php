afa2405291b9518735c3fecd758ed44641d6bc14
thientrankhh <thientrankhh@gmail.com>
2020-04-24 11:41:23 +0700

    Migration and seeder

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['prefix'=>'admin','as'=>'admin.', 'namespace' => 'Admin'], function () {
+    Route::get('/','LoginController@showLogin')->name('showLogin');
+
+
+});
