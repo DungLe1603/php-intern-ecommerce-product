1f355ec8b4400a5cebe009d028dffaebc54e9ef3
thientrankhh <thientrankhh@gmail.com>
2020-04-28 09:57:57 +0700

    Done Update Product Admin

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -14,4 +14,29 @@ class ProductController extends Controller
 
         return view('admin.product.list_products', compact('products'));
     }
+
+    public function editProduct($id)
+    {
+        $product = Product::where('id', $id)->first();
+
+        return view('admin.product.editProduct', compact('product'));
+    }
+
+    public function updateProduct(Request $request, $id)
+    {
+        $params = $request->all();
+        $dataUp = [
+            'product_name' => $params['product_name'],
+            'quantity' => $params['quantity'],
+            'description' => $params['description'],
+            'configuration' => $params['configuration'],
+            'colors' => $params['colors'],
+            'price' => $params['price'],
+            'created_at' => $params['created_at'],
+            'updated_at' => $params['updated_at']
+        ];
+        Product::where('id', $id)->update($dataUp);
+
+        return redirect()->route('admin.listAllProducts')->with('success', 'Update Success');
+    }
 }
